33ab160975511b7e67a386c14d7ea8502c0d0f1b
Payal Choudhary <xaverianpayalme@gmail.com>
2020-05-26 17:05:18 +0530

    DialogFragment

--- app/src/main/java/com/example/infi_project/data/model/ProfileImagePicker.java
+++ app/src/main/java/com/example/infi_project/data/model/ProfileImagePicker.java
@@ -132,6 +132,8 @@ public class ProfileImagePicker extends DialogFragment {
 
                 Uri resultUri = result.getUri();
 
+                //Meow
+
 
 
                 StorageReference filePath = userProfileImagesReference.child(mobileText + ".jpg");
